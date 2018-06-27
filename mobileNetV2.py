1069ea38d97c72923963656ccac4d25a0a3b67c4
oceantear <oceantear@gmail.com>
2018-06-27 11:23:00 +0800

    mobilenetV2

---
+++ mobileNetV2.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python
+import os
+import sys
+import argparse
+import pandas as pd
+import keras
+from keras_applications.mobilenet_v2 import MobileNetV2
+from keras_applications.mobilenet import MobileNet
+from keras.optimizers import Adam
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import EarlyStopping
+from keras.layers import Conv2D, Reshape, Activation
+from keras.models import Model
+from keras.layers import Input, Dense, GlobalAveragePooling2D
+
+image_size = 224
+
+train_dir = '/home/advrobot/keras_Fine_Tuning_VGG16/train'
+validation_dir = '/home/advrobot/keras_Fine_Tuning_VGG16/test'
+save_model_name = 'mobileNet_ex1.h5'
+
+#model = MobileNetV2(weights='imagenet', include_top=False, input_shape=(image_size, image_size, 3))
+model = MobileNetV2(weights='imagenet',include_top=True ,input_shape=(image_size, image_size, 3))
+#model.load_weights("imagenet")
+
+'''
+        MobileNetV1
+        x = layers.GlobalAveragePooling2D()(x)
+        x = layers.Reshape(shape, name='reshape_1')(x)
+        x = layers.Dropout(dropout, name='dropout')(x)
+        x = layers.Conv2D(classes, (1, 1),
+                          padding='same',
+                          name='conv_preds')(x)
+        x = layers.Activation('softmax', name='act_softmax')(x)
+        x = layers.Reshape((classes,), name='reshape_2')(x)
+
+
+        x = base_model.output
+        x = GlobalAveragePooling2D()(x)
+        x = Dense(1024, activation='relu')(x)
+        predictions = Dense(2, activation='softmax')(x)
+'''
+
+'''
+    if include_top:
+        x = layers.GlobalAveragePooling2D()(x)
+        x = layers.Dense(classes, activation='softmax',
+                         use_bias=True, name='Logits')(x)
+
+    
+    x = Conv2D(num_classes, (1, 1), padding='same')(x)
+    x = Activation('softmax', name='softmax')(x)
+    output = Reshape((num_classes,))(x)
+
+    model = Model(inputs=model.input, outputs=output)
+'''
+x = model.output
+x = GlobalAveragePooling2D()(x)
+predictions = Dense(2, activation='softmax')(x)
+model = Model(inputs=model.input, outputs=predictions)
+model.summary()
+
+
+
+
+#x = model.get_layer('Dropout').output
+
+#x.summary()
+
+#model2 = MobileNetV2(weights='imagenet', include_top=False, input_shape=(image_size, image_size, 3))
+#model2.summary()
+
+for layer in model.layers:
+    print(layer, layer.trainable)
+
+# No Data augmentation 
+train_datagen = ImageDataGenerator(rescale=1./255)
+validation_datagen = ImageDataGenerator(rescale=1./255)
+
+# Change the batchsize according to your system RAM
+train_batchsize = 20
+val_batchsize = 10
+
+# Data Generator for Training data
+train_generator = train_datagen.flow_from_directory(
+        train_dir,
+        target_size=(image_size, image_size),
+        batch_size=train_batchsize,
+        class_mode='categorical')
+
+# Data Generator for Validation data
+validation_generator = validation_datagen.flow_from_directory(
+        validation_dir,
+        target_size=(image_size, image_size),
+        batch_size=val_batchsize,
+        class_mode='categorical',
+        shuffle=False)
+
+# Compile the model
+model.compile(loss='categorical_crossentropy',
+              optimizer=optimizers.RMSprop(lr=1e-4),
+              metrics=['acc'])
+
+print("start training time = ",str(datetime.now()))
+
+# Train the Model
+history = model.fit_generator(
+      train_generator,
+      steps_per_epoch=train_generator.samples/train_generator.batch_size ,
+      epochs=20,
+      validation_data=validation_generator,
+      validation_steps=validation_generator.samples/validation_generator.batch_size,
+      verbose=2)
+
+# Save the Model
+model.save(save_model_name)
+print("end training time = ",str(datetime.now()))
+
+# Plot the accuracy and loss curves
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'b', label='Training acc')
+plt.plot(epochs, val_acc, 'r', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'b', label='Training loss')
+plt.plot(epochs, val_loss, 'r', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
+
+
+# Create a generator for prediction
+validation_generator = validation_datagen.flow_from_directory(
+        validation_dir,
+        target_size=(image_size, image_size),
+        batch_size=val_batchsize,
+        class_mode='categorical',
+        shuffle=False)
+
+# Get the filenames from the generator
+fnames = validation_generator.filenames
+
+# Get the ground truth from generator
+ground_truth = validation_generator.classes
+
+# Get the label to class mapping from the generator
+label2index = validation_generator.class_indices
+
+# Getting the mapping from class index to class label
+idx2label = dict((v,k) for k,v in label2index.items())
+
+# Get the predictions from the model using the generator
+predictions = model.predict_generator(validation_generator, steps=validation_generator.samples/validation_generator.batch_size,verbose=2)
+predicted_classes = np.argmax(predictions,axis=1)
+
+errors = np.where(predicted_classes != ground_truth)[0]
+print("No of errors = {}/{}".format(len(errors),validation_generator.samples))
+
+# Show the errors
+for i in range(len(errors)):
+    pred_class = np.argmax(predictions[errors[i]])
+    pred_label = idx2label[pred_class]
+    
+    title = 'Original label:{}, Prediction :{}, confidence : {:.3f}'.format(
+        fnames[errors[i]].split('/')[0],
+        pred_label,
+        predictions[errors[i]][pred_class])
+    
+    original = load_img('{}/{}'.format(validation_dir,fnames[errors[i]]))
+    plt.figure(figsize=[7,7])
+    plt.axis('off')
+    plt.title(title)
+    plt.imshow(original)
+    plt.show()
